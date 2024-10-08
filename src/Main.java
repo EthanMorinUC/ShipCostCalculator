e1b6be9f4ceb187d58de22923a42b544805df494
EthanMorinUC <morinej@mail.uc.edu>
2024-10-07 22:29:03 -0400

    Completed

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,7 @@
 import java.util.Scanner;
 
-public class Main
-{
-    public static void main(String[] args)
-    {
+public class Main {
+    public static void main(String[] args) {
         Scanner console = new Scanner(System.in);
         double itemPrice = 0;
         final double SHIP_RATE = 0.02;
@@ -13,26 +11,20 @@ public class Main
         String trash = "";
 
         System.out.println("Enter the item price: ");
-        if(console.hasNextDouble())
-        {
+        if (console.hasNextDouble()) {
             itemPrice = console.nextDouble();
             console.nextLine();
 
-            if(itemPrice >= FREE_SHIP)
-            {
+            if (itemPrice >= FREE_SHIP) {
                 shipCost = 0;
                 totalCost = itemPrice;
-            }
-            else
-            {
+            } else {
                 shipCost = itemPrice * SHIP_RATE;
                 totalCost = itemPrice + shipCost;
             }
             System.out.println("The ship cost is " + shipCost);
             System.out.println("The total cost is " + totalCost);
-        }
-        else
-        {
+        } else {
             trash = console.nextLine();
             System.out.println("You entered " + trash + " not a number!");
             System.out.println("Run the program again with the correct input.");
